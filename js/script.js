8ece9d0c4adfc614ab518f52f04be4306066e3f7
Milton-web <milton.website@gmail.com>
2018-06-05 19:10:07 -0500

    se inicializo el repositorio - Page Surf

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let btnMenu = document.getElementById('btn-menu');
+let contentMenu = document.getElementById('main-nav');
+  btnMenu.addEventListener('click', function(){
+    contentMenu.classList.toggle('show-menu')
+  })
