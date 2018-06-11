b989d1a64395bad8dedaa4a55dc3a56bc8f25eeb
Milton-web <milton.website@gmail.com>
2018-06-11 17:00:11 -0500

    nueva funcion del menu se agrego transition al menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let btnMenu = document.getElementById('btn-menu');
-let contentMenu = document.getElementById('main-nav');
+let contentMenu = document.getElementById('main-menu');
   btnMenu.addEventListener('click', function(){
     contentMenu.classList.toggle('show-menu')
   })
